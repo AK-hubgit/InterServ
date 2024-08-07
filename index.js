9051284f151a7aa526b8dc94bf1e542891057cc0
AK-hubgit <ak0234168@gmail.com>
2024-08-07 21:45:53 +0530

    07-08-2024 21:45 updates

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
     var swiper = new Swiper(".mySwiper", {
       spaceBetween: 30,
       centeredSlides: true,
-    //   autoplay: {
-    //     delay: 1500,
-    //     disableOnInteraction: false,
-    //   },
+      autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+      },
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
